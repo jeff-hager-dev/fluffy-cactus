4cc0771330ff7e54db1cdc07486af223a965f62b
Adam Frank <adam.frank@daugherty.com>
2016-08-19 12:52:14 -0500

    added status

--- elevator.js
+++ elevator.js
@@ -4,48 +4,57 @@
 var possibleDirs = {"UP": 1, "STILL": 0, "DOWN": -1};
 
 class elevator {
-	constructor() {
-		this.destFlr = -1;
-		this.curFlr = -1;
-		this.maxPpl = -1;
-		this.numPpl = -1;
-		this.curDir = possibleDirs.STILL;
-		this.people = [];
-        this.name = 1;
-	};
 
-	pickupPerson = function (person) {
-		if (this.numPpl < this.maxPpl) {
+    var status = {"AT_FLOOR": 1, "BETWEEN_FLOORS": 2, "ON_FIRE": 3};
+
+    constructor() {
+
+        this.destFlr = -1;
+        this.curFlr = -1;
+        this.maxPpl = -1;
+        this.numPpl = -1;
+        this.curDir = possibleDirs.STILL;
+        this.people = [];
+        this.name = 1;
+        this.status = status.AT_FLOOR;
+
+    };
+
+    pickupPerson = function (person) {
+        this.status = status.AT_FLOOR;
+        if (this.numPpl < this.maxPpl) {
             this.people.push(person);
-            this.numPpl+=1;
-			if (this.destFlr === -1) {
-				this.destFlr = person.destFlr;
+            this.numPpl += 1;
+            if (this.destFlr === -1) {
+                this.destFlr = person.destFlr;
                 this.setDir(this.destFlr);
-			}
-		}
+            }
+        }
         else {
             throw new Error("Elevator " + this.name + "is full.");
         }
-	};
+
+    };
 
 
-    dropPerson = function(person) {
-      this.people = people.filter(function(p) {
-          return p.callId != person.callId;
-      });
+    dropPerson = function (person) {
+        this.people = people.filter(function (p) {
+            return p.callId != person.callId;
+        });
         this.numPpl -= 1;
-        if(this.numPpl === 0) {
+        if (this.numPpl === 0) {
             this.curDir = possibleDirs.STILL;
         }
     };
 
-    move = function(dir) {
+    move = function (dir) {
         this.curFlor += possibleDirs[dir];
+        this.status = status.BETWEEN_FLOORS;
     };
 
 
-    setDir = function(flr) {
-        if(flr < this.curFlr) {
+    setDir = function (flr) {
+        if (flr < this.curFlr) {
             this.curDir = possibleDirs.DOWN;
         }
 
