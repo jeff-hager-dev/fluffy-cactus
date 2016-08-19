837fe859722c861c863bccb4f9fd6cec99f820d1
Meuth <CAM0729@daugherty.com>
2016-08-19 12:01:39 -0500

    Made people

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 (function(){
+    'use strict';
+
+    var Person = require('./person');
     var poolPeople = [];
     var peopleWaiting = [];
     var poolOfElevators = [];
     var numTimeIncrement = 1;
+    var data = require('./data/challenge1input.json');
 
-    
+    for(var i = 0; i < data.calls.length; i++) {
+        var call = data.calls[i];
+        var newPerson = new Person(call.callId, call.callTime, call.startFloor, call.endFloor);
+        poolPeople.push(newPerson);
+        console.log(poolPeople);
+    }
+
+    // console.log(data);
 
     var getPersonFromPool = function(time, poolPeople){};
     var updateElevators = function (time, poolWaiting, poolOfElevators) {
@@ -12,12 +23,11 @@
     };
 
     var totalTimePast = 0;
-
+/*
     while(poolPeople.length < 0){
         peopleWaiting.push(getPersonFromPool(totalTimePast, poolPeople));
         updateElevators(peopleWaiting, poolOfElevators);
         totalTimePast+=numTimeIncrement;
-    }
-
+    }*/
 
 })();
